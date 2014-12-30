9a843696d750e20fbd098259047231a9500620a2
Liu Jin <coolziljin@gmail.com>
2014-12-30 15:02:25 +0800

    fix infinite loop in layoutSubviews

--- XTNews/TopAligningLabel/MCTopAligningLabel.m
+++ XTNews/TopAligningLabel/MCTopAligningLabel.m
@@ -16,8 +16,24 @@
     [self setNeedsLayout];
 }
 
-- (void)layoutSubviews
-{
+// http://stackoverflow.com/questions/24510596/xcode6-ios8-and-voidlayoutsubviews
+// http://stackoverflow.com/questions/25789854/layoutsubviews-in-infinite-loop-on-ios-8-gm
+// BUG: 估计是在 layoutSubviews 里调用 setFrame 触发了 layoutSubviews 造成了死循环。
+
+//- (void)layoutSubviews
+//{
+//    CGSize size = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(self.bounds.size.width, CGFLOAT_MAX)];
+//    CGAffineTransform transform = self.transform;
+//    self.transform = CGAffineTransformIdentity;
+//    CGRect frame = self.frame;
+//    frame.size.height = size.height;
+//    self.frame = frame;
+//    self.transform = transform;
+//    
+//    [super layoutSubviews];
+//}
+
+- (void)didAddSubview:(UIView *)subview {
     CGSize size = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(self.bounds.size.width, CGFLOAT_MAX)];
     CGAffineTransform transform = self.transform;
     self.transform = CGAffineTransformIdentity;
@@ -25,8 +41,6 @@
     frame.size.height = size.height;
     self.frame = frame;
     self.transform = transform;
-    
-    [super layoutSubviews];
 }
 
 @end
